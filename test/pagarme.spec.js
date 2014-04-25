0178e593e91a087f11ac32e839d41cd16d917d78
Felipe Djinn <lfrs.web@gmail.com>
2014-04-25 16:51:58 -0300

    Add test structure

---
+++ test/pagarme.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var PagarMe = require('../lib/pagarme');
+
+describe('PagarMe', function() {
+
+    beforeEach(function() {
+        PagarMe.apiKey = 'ak_test_Rw4JR98FmYST2ngEHtMvVf5QJW7Eoo';;
+    });
+
+});
+
+
