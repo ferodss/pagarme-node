6853c16ab85c795094fe621a8bdea9275fb3ba74
Felipe Rodrigues <lfrs.web@gmail.com>
2014-04-26 18:40:31 +0000

    Format main PagarMe namespace

--- test/pagarme.spec.js
+++ test/pagarme.spec.js
@@ -1,11 +1,29 @@
 'use strict';
 
-var PagarMe = require('../lib/pagarme');
 
 describe('PagarMe', function() {
 
+    var apiKey = 'ak_test_Rw4JR98FmYST2ngEHtMvVf5QJW7Eoo',
+        PagarMe;
+
     beforeEach(function() {
-        PagarMe.apiKey = 'ak_test_Rw4JR98FmYST2ngEHtMvVf5QJW7Eoo';;
+        PagarMe = require('../lib/pagarme')
+    });
+
+
+    it('should be able to set the API key', function() {
+        PagarMe.apiKey = apiKey;
+        expect(PagarMe.apiKey).toBe(apiKey);
+    });
+
+    it('endpoint should be defined', function() {
+        expect(PagarMe.endpoint).toBeDefined();
+        expect(PagarMe.endpoint).not.toBeNull();
+    });
+
+    it('API version should be defined', function() {
+        expect(PagarMe.apiVersion).toBeDefined();
+        expect(PagarMe.apiVersion).not.toBeNull();
     });
 
 });
