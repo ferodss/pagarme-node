48b44770856aa94a8e1712f134825a3d9bfbd4d5
Felipe Rodrigues <lfrs.web@gmail.com>
2014-04-28 04:57:47 +0000

    Add support to chage a transaction

--- test/transaction.spec.js
+++ test/transaction.spec.js
@@ -1,12 +1,15 @@
 'use strict';
 
-describe('PagarMe::Transsaction', function() {
+describe('PagarMe::Transaction', function() {
 
     var PagarMe
+    , apiKey = 'ak_test_Rw4JR98FmYST2ngEHtMvVf5QJW7Eoo'
     , params = {};
 
     beforeEach(function() {
         PagarMe = require('../lib/pagarme');
+        PagarMe.apiKey = apiKey;
+
         params = {
             cardNumber: '4901720080344448',
             cardHolderName: 'Jose da Silva',
@@ -33,12 +36,19 @@ describe('PagarMe::Transsaction', function() {
         expect(transaction.amount).toBe(params.amount);
     });
 
-    it('should be able to charge', function() {
+    it('should be able to charge', function(done) {
         var transaction = new PagarMe.Transaction(params);
         expect(transaction.status).toBe('local');
+        expect(typeof transaction.charge).toBe('function');
 
-        transaction.charge();
-        expect(transaction.status).toBe('paid');
+        transaction.charge(function() {
+            expect(transaction.id).not.toBeNull();
+            expect(transaction.status).toBe('paid');
+            expect(transaction.refuseReason).toBeNull();
+            expect(transaction.dateCreated).not.toBeNull();               
+
+            done();
+        });
     });
 
 });
